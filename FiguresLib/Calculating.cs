a518a5d1b5ce3b83ceb72bea3d4643a4076e3cf8
Irbistomin Dmitry <irbistomin@gmail.com>
2021-08-04 18:29:29 +0300

    Добавьте файлы проекта.

---
+++ FiguresLib/Calculating.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FiguresLib
+{
+    public class Calculating
+    {
+        public double calculating(double r)
+        {
+            double d = r + r, s;
+            s = Math.PI * (r*r);
+            return s;
+        }
+        public double calculating(double x1,double x2, double x3)
+        {
+            double p = 0, s = 0;
+            p = (x1 + x2 + x3) / 2;
+            s = Math.Sqrt(p * (p - x1) * (p - x2) * (p - x3));
+            return s;
+        }
+
+
+   
+    }
+}
+/*
+Формула площади круга: Площадь круга ,
+где r - радиус круга, d - диаметр
+*/
---
+++ TaskSFigures/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FiguresLib;
+
+namespace TaskSFigures
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Calculating calc = new Calculating();
+                tb2.Text = calc.calculating(double.Parse(tb1.Text)).ToString("0.0000");
+            }
+            catch (Exception exp) { MessageBox.Show("Что то пошло не так: " + exp.Message, "Ошибка"); }
+        }
+
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double a = Convert.ToDouble(a1.Text), b = Convert.ToDouble(a2.Text), c = Convert.ToDouble(a3.Text);
+                Calculating calc = new Calculating();
+                tb3.Text = calc.calculating(a, b, c).ToString("0.0000");
+                if ((c * c) == (a * a) + (b * b))
+                {
+                    tb4.Text = "Треугольник является прямоугольным";
+                } else tb4.Text = "Треугольник не является прямоугольным";
+            }
+            catch (Exception exp) { MessageBox.Show("Что то пошло не так(2): "+exp.Message,"Ошибка"); }
+
+            }
+    }
+}
